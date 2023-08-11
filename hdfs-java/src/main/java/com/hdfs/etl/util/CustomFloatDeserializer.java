8256b0710f26166116af8a7c75e7b83c564a9ce0
Seungyeup <lsyeup1206@naver.com>
2023-08-11 12:58:42 +0900

    feat: float deserializer

--- hdfs-java/src/main/java/com/hdfs/etl/util/CustomFloatDeserializer.java
+++ hdfs-java/src/main/java/com/hdfs/etl/util/CustomFloatDeserializer.java
@@ -7,11 +7,17 @@ import org.codehaus.jackson.map.JsonDeserializer;
 
 import java.io.IOException;
 
+// FRED 에서 값을 불러올 때 가끔 숫자가 아닌 `.`값만 반환되는 경우가 있다.
 public class CustomFloatDeserializer extends JsonDeserializer<Float> {
 
     @Override
     public Float deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
-        return null;
+        // TODO : Auto-Generated Method stub
+        String floatString = jp.getText();
+        if(floatString.equals(".")){
+            return null;
+        }
+        return Float.valueOf(floatString);
     }
 
 }
