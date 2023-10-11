d078317b4298b6573fb16776565f5c11a936a720
Seungyeup <lsyeup1206@naver.com>
2023-10-11 12:45:01 +0900

    fix:     - container volume     - not working containers(namenode, resouremanager) todo:     - cluster is still unhealty..

--- hadoop-cluster-setup-ver2/start-cluster.sh
+++ hadoop-cluster-setup-ver2/start-cluster.sh
@@ -2,6 +2,6 @@
 
 docker network create hadoop_cluster_network
 
-# docker build -t lsyeup1206/hadoop_cluster_base .
+docker build -t lsyeup1206/hadoop_cluster_base -f Dockerfile-Hadoop-Cluster .
 
-docker-compose up -d
+docker-compose -f docker-compose-hadoop-cluster.yml up -d
