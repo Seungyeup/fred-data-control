89ee586326c024238e9289bae9b4fa5cf296bf34
Seungyeup <lsyeup1206@naver.com>
2023-10-18 12:27:18 +0900

    single datanode cluster

--- hadoop-cluster-setup-ver2/start-cluster.sh
+++ hadoop-cluster-setup-ver2/start-cluster.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+rm -rf ~/local-docker-volume/hadoop_cluster/*
+mkdir -p ~/local-docker-volume/hadoop_cluster/hadoop_namenode
+mkdir -p ~/local-docker-volume/hadoop_cluster/hadoop_datanode1
+mkdir -p ~/local-docker-volume/hadoop_cluster/hadoop_datanode2
+mkdir -p ~/local-docker-volume/hadoop_cluster/hadoop_datanode3
+
 docker network create hadoop_cluster_network
 
 docker build -t lsyeup1206/hadoop_cluster_base -f Dockerfile-Hadoop-Cluster .
